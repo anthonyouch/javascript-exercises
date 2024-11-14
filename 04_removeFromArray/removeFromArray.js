42c0297a411aa9565c4c57edd3f6cce4342c6db2
anthonyouch <anthonyouch.programmer@gmail.com>
2024-11-14 19:28:02 +1100

    completed 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...nums) {
+    finalArr = [];
+    for (val of arr ) {
+        let addNum = true;
+        for (num of nums) {
+            if (val === num) {
+                addNum = false;
+            }
+        }
+        if (addNum) {
+            finalArr.push(val);
+        }
+    }
+    return finalArr;
 };
 
 // Do not edit below this line
